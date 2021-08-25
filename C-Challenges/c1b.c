4ff8d0387aa0035de5a6eb5695433a5b8d3ca109
ub-oh <bariskaplan22@gmail.com>
2021-08-25 17:43:28 +0300

    challenge 1: ub-oh

---
+++ C-Challenges/c1b.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main()
+{
+    printf("Name: Baris Kaplan\n");
+    printf("DOB: June 22, 2000\n");
+    printf("Mobile: 99-9999999999\n");
+    getchar();
+    return 0;
+}
