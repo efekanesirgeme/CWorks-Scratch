a18d819cef8a7a965f7803f626d0a8f45a0e3ff4
efekanesirgeme <36383675+efekanesirgeme@users.noreply.github.com>
2021-08-25 17:29:17 +0300

    first challenge commit1

---
+++ C- Challenges/C1E.c
@@ -0,0 +1,13 @@
+// This is our first Challenge for learning C, our source is: https://www.w3resource.com/
+
+#include <stdio.h>
+
+int main()
+{
+
+printf("Name: Efekan");
+printf("Dob: July 29, 1999");
+printf("Phone: 000000000");
+return 0;
+
+}
