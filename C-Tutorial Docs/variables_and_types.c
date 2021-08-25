77881c7df2b6fdeed19792fac50a0c042f731e63
efekanesirgeme <36383675+efekanesirgeme@users.noreply.github.com>
2021-08-25 18:07:10 +0300

    new lines

--- C-Tutorial Docs/variables_and_types.c
+++ C-Tutorial Docs/variables_and_types.c
@@ -1,2 +1,14 @@
 #include <stdio.h>
 
+/*
+integer data types:
+
+char: 8 bit
+int: 16 bit
+long/long int: 32 bit
+long long/long long int: 64 bit
+
+floating point data types:
+float: usually 32 bit
+double: usually 64 bit
+*/
